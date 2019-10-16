fe55c5a0d20ea4028c4b5457819c0b7e496ab8a6
MaxGentile <55592061+MaxGentile@users.noreply.github.com>
2019-10-16 20:17:29 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
-// Server Node.js 
-
-var express = require('express');//abilita express
-var path = require('path');//abilita path
+import express, { static } from 'express';//abilita express
+import { join } from 'path';//abilita path
 var port = process.env.PORT || 3000;
 var app = express();
 
@@ -10,10 +8,10 @@ app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   next();
 });
-app.use(express.static(__dirname + '/public'));
+app.use(static(__dirname + '/public'));
 // imposta la route
-app.get('/', (req, res,next) => {
-    res.sendFile(path.join(__dirname, 'public/index.html'));
+app.get('*', (req, res,next) => {
+    res.sendFile(join(__dirname, 'public/index.html'));
 });
   
   var server = http.createServer(app);
