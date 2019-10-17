9034cbce78bcab218fb61ce0a4906de213b75a8a
MaxGentile <55592061+MaxGentile@users.noreply.github.com>
2019-10-17 07:18:09 +0200

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   next();
 });
-app.use(express.static(__dirname + '/public'));
+app.use('/books-download', express.static(__dirname + '/public'));
 // imposta la route
 app.get('*', (req, res,next) => {
     res.sendFile(path.join(__dirname, 'public/index.html'));
