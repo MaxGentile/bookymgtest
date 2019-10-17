065cf84e925931cd53bc88bead48c082d4232ee7
MaxGentile <55592061+MaxGentile@users.noreply.github.com>
2019-10-17 08:12:37 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,20 +1,18 @@
-var express = require('express');//abilita express
+var express = require('express'); //abilita express
 var http = require('http'); //abilita http
-var path = require('path');//abilita path
+var path = require('path') ;//abilita path
+var cors = require('cors'); //abilita cors
 var port = process.env.PORT || 3000;
 var app = express();
 
 // Imposta la struttura delle cartelle
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  next();
-});
-app.use('/books-download', express.static(__dirname + '/public'));
+app.use(cors());
+app.use('/', express.static(__dirname + '/public'));
 // imposta la route
 app.get('*', (req, res,next) => {
-    res.sendFile(path.join(__dirname, 'public/index.html'));
+    res.sendFile(path.join(__dirname, 'public/index.html'))
 });
   
-  var server = http.createServer(app);
+var server = http.createServer(app);
 server.listen(port);
 console.log("Il criceto gira sulla porta " + port);
