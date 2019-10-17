c588e20829008f5b2cfa5b03ce62af04fa0baddf
MaxGentile <55592061+MaxGentile@users.noreply.github.com>
2019-10-17 08:34:21 +0200

    Update server.js

--- server.js
+++ server.js
@@ -6,10 +6,10 @@ var port = process.env.PORT || 3000;
 var app = express();
 
 // Imposta la struttura delle cartelle
-app.use(cors());
+
 app.use('/', express.static(__dirname + '/public'));
 // imposta la route
-app.get('*', (req, res,next) => {
+app.get('*', cors(), (req, res,next) => {
     res.sendFile(path.join(__dirname, 'public/index.html'))
 });
   
