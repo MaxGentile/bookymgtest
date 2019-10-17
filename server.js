6ae9690fa6d06206a4a58637bb83d5845ea4c658
MaxGentile <55592061+MaxGentile@users.noreply.github.com>
2019-10-17 11:12:08 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -1,18 +1,21 @@
-var express = require('express'); //abilita express
-var http = require('http'); //abilita http
-var path = require('path') ;//abilita path
-var cors = require('cors'); //abilita cors
+// Server Node.js 
+
+var express = require('express');//abilita express
+var http = require('http');//abilita http
+var path = require('path');//abilita path
 var port = process.env.PORT || 3000;
+var cors = require('cors'); //abilita cors
 var app = express();
 
 // Imposta la struttura delle cartelle
+app.use(express.static(__dirname + '/public'));
+app.use(cors({ origin: 'https://bookymgunimi.netsons.org/xample.com' }));
 
-app.use('/', express.static(__dirname + '/public'));
 // imposta la route
-app.get('*', cors(), (req, res,next) => {
-    res.sendFile(path.join(__dirname, 'public/index.html'))
+app.get('/', (req, res,next) => {
+    res.sendFile(path.join(__dirname, 'public/index.html'));
 });
-  
+ 
 var server = http.createServer(app);
 server.listen(port);
 console.log("Il criceto gira sulla porta " + port);
